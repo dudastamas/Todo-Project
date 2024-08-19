df758e7021b509519e11e9cca246e4affcee4b65
dudastamas <tamasdudas230@gmail.com>
2024-08-19 20:13:19 +0200

    Kód egyszerűsítve

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -8,11 +8,11 @@ export default function Content({project, onAddProject,  }) {
     return (
         <div className="flex-1 bg-white h-screen p-8">
           <ProjectForm onAddProject={onAddProject}/>
-          {project ? (
+          {project &&(
             <div>
             <p>Task: {project.task}</p>
             <p>Date: {project.date}</p>
-          </div>) : "Kérlek válassz projectet"}
+          </div>)}
         </div>
       );
   
